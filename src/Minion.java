efc16122fce978f4d8f984a3a56734b205ac30a6
league <league@wts-im-17.attlocal.net>
2018-08-25 15:57:19 -0700

    awsd

--- src/Minion.java
+++ src/Minion.java
@@ -1,9 +1,29 @@
 
 public class Minion {
-	MinionTest stuart = new MinionTest();
-	
-	
-	
-	
-	
+	String name;
+	int numeye;
+	String color;
+	String Master;
+	public Minion(String name, int numeye, String color, String Master){
+	this.name=name;
+	this.numeye=numeye;
+	this.color=color;
+	this.Master=Master;
 	}
+	public String getMaster() {
+		return Master;
+	}
+	public String getColor() {
+		return color;
+	}
+public String getName() {
+	return name;
+}
+public int getNumeye() {
+	return numeye;
+}
+public void setColor(String color) {
+	this.color = color;
+}
+
+}
